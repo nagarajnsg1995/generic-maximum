ee01a75a8699ac8be23fa8d6e61ee4f72bf2ac0e
nagarajnsg1995 <nagarajaece.rymec@gmail.com>
2022-06-18 22:24:33 +0530

    Added:finding the maximum integer,float,string using sort method in generic

--- src/com/bridgelabz/MaximumData.java
+++ src/com/bridgelabz/MaximumData.java
@@ -1,35 +1,17 @@
 package com.bridgelabz;
 
-class GenericData<T extends Comparable> {
-    T a, b, c;
 
-    public GenericData(T a, T b, T c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-
-    public T maximum() {
-        T max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        return max;
-    }
-}
-
+import java.util.Arrays;
 
 public class MaximumData {
+    public static <T> T maximum(T... a) {
+        Arrays.sort(a);
+        return a[a.length - 1];
+    }
 
     public static void main(String[] args) {
-        GenericData<Integer> genericData = new GenericData<>(4, 9, 3);
-        GenericData<Float> genericData1 = new GenericData<>(1.7f, 13.1f, 17.2f);
-        GenericData<String> genericData2 = new GenericData<>("apple", "banana", "mango");
-        System.out.println("maximum integer number is: " + genericData.maximum());
-        System.out.println("maximum float number is: " + genericData1.maximum());
-        System.out.println("maximum string name is: " + genericData2.maximum());
+        System.out.println("maximum integer number is: " + maximum(3, 4, 8, 13, 56, 28, 22));
+        System.out.println("maximum flot number is: " + maximum(1.2f, 2.6f, 8.6f, 4.2f, 4.1f));
+        System.out.println("maximum string name is: " + maximum("apple", "banana", "coconut", "dates", "grape", "guava"));
     }
 }
