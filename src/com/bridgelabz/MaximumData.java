bb93804328042dea4c29ad07156bde4596d5911c
nagarajnsg1995 <nagarajaece.rymec@gmail.com>
2022-06-18 19:21:10 +0530

    Added:finding the maximum string name

--- src/com/bridgelabz/MaximumData.java
+++ src/com/bridgelabz/MaximumData.java
@@ -7,30 +7,47 @@ public class MaximumData {
             max = b;
         }
         if (c.compareTo(max) > 0) {
-            max=c;
+            max = c;
 
         }
 
         return max;
     }
+
     public static Float maximumFloat(Float a, Float b, Float c) {
         Float max = a;
         if (b.compareTo(max) > 0) {
             max = b;
         }
         if (c.compareTo(max) > 0) {
-            max=c;
+            max = c;
 
         }
 
         return max;
     }
 
+    public static String maximumString(String a, String b, String c) {
+        String max = a;
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+
+        }
+
+        return max;
+    }
+
+
     public static void main(String[] args) {
-        Integer resultOne =  maximumInt(10, 5, 8);
-        Float resultTwo = maximumFloat((float) 10.7, (float)5.4, (float)18.6);
-        System.out.println("maximum integer number is: " +resultOne);
-        System.out.println("maximum float number is: " +resultTwo);
+        Integer resultOne = MaximumData.maximumInt(10, 5, 8);
+        Float resultTwo = MaximumData.maximumFloat(10.7f, 5.4f, 18.6f);
+        String resultThree = MaximumData.maximumString("apple", "peach", "orange");
+        System.out.println("maximum integer number is: " + resultOne);
+        System.out.println("maximum float number is: " + resultTwo);
+        System.out.println("maximum String name is:" + resultThree);
     }
 
 }
