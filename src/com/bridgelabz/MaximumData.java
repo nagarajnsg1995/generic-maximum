88a07243ddee58e4bd49bd6d10217531f0a445a7
nagarajnsg1995 <nagarajaece.rymec@gmail.com>
2022-06-18 19:00:44 +0530

    Added:finding maximum float number

--- src/com/bridgelabz/MaximumData.java
+++ src/com/bridgelabz/MaximumData.java
@@ -13,10 +13,24 @@ public class MaximumData {
 
         return max;
     }
+    public static Float maximumFloat(Float a, Float b, Float c) {
+        Float max = a;
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max=c;
+
+        }
+
+        return max;
+    }
 
     public static void main(String[] args) {
-        Integer result =  maximumInt(10, 5, 8);
-        System.out.println("maximum integer is:" +result);
+        Integer resultOne =  maximumInt(10, 5, 8);
+        Float resultTwo = maximumFloat((float) 10.7, (float)5.4, (float)18.6);
+        System.out.println("maximum integer number is: " +resultOne);
+        System.out.println("maximum float number is: " +resultTwo);
     }
 
 }
