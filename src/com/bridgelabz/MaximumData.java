8636e91532f673d302c6cb0f98abc68135a71ea0
nagarajnsg1995 <nagarajaece.rymec@gmail.com>
2022-06-18 20:02:23 +0530

    Added:refactor the code in to generic

--- src/com/bridgelabz/MaximumData.java
+++ src/com/bridgelabz/MaximumData.java
@@ -1,53 +1,23 @@
 package com.bridgelabz;
 
 public class MaximumData {
-    public static Integer maximumInt(Integer a, Integer b, Integer c) {
-        Integer max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
+   public static <T extends Comparable>T maximum(T a, T b, T c) {
+      T max = a;
+      if(b.compareTo(max)>0){
+         max = b;
+      }
+      if(c.compareTo(max)>0){
+         max = c;
+      }
+      return max;
+   }
 
-        }
-
-        return max;
-    }
-
-    public static Float maximumFloat(Float a, Float b, Float c) {
-        Float max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-
-        }
-
-        return max;
-    }
-
-    public static String maximumString(String a, String b, String c) {
-        String max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-
-        }
-
-        return max;
-    }
-
-
-    public static void main(String[] args) {
-        Integer resultOne = MaximumData.maximumInt(10, 5, 8);
-        Float resultTwo = MaximumData.maximumFloat(10.7f, 5.4f, 18.6f);
-        String resultThree = MaximumData.maximumString("apple", "peach", "orange");
-        System.out.println("maximum integer number is: " + resultOne);
-        System.out.println("maximum float number is: " + resultTwo);
-        System.out.println("maximum String name is:" + resultThree);
-    }
-
+   public static void main(String[] args) {
+      Integer resultOne = MaximumData.maximum(10, 5, 8);
+      Float resultTwo = MaximumData.maximum(10.7f, 5.4f, 18.6f);
+      String resultThree = MaximumData.maximum("apple", "peach", "orange");
+      System.out.println("maximum integer number is:" +resultOne);
+      System.out.println("maximum float number is:" +resultTwo);
+      System.out.println("maximum string name is:" +resultThree);
+   }
 }
